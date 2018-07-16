4fc73740f679214fa005a27d6f90bf811e39a808
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-07-16 19:55:08 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -19,4 +19,11 @@ class LandmarksController < ApplicationController
     erb :'/landmarks/edit'
   end
 
+  post '/landmarks/:id/edit' do
+    @landmark = Landmark.find_by_id(params[:id])
+    @landmark.update(name: params[:name], year_completed: params[:year_completed])
+    @landmark.save
+    redirect "/landmarks/#{@landmark.id}"
+  end
+
 end
