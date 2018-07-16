5fd0d01492abe8f7d9fc724571500294ce48d039
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-07-16 19:13:29 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -33,7 +33,7 @@ class FiguresController < ApplicationController
     erb :'/figures/edit'
   end
 
-  post '/figures/:id' do
+  patch '/figures/:id' do
     #binding.pry
     @figure = Figure.find_by_id(params[:id])
     @figure.update(params[:figure])
